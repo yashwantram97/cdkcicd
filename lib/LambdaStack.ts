5fd2b0c9eadd24b4afcc1a059be44e7e18f151bc
yash <yash97r@gmail.com>
2024-07-26 00:11:15 +0530

    Adding lambda api

--- lib/LambdaStack.ts
+++ lib/LambdaStack.ts
@@ -1,5 +1,8 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import { join } from "path";
 
 interface LambdaStackProps extends StackProps {
   stageName?: string;
@@ -8,5 +11,14 @@ interface LambdaStackProps extends StackProps {
 export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: LambdaStackProps) {
     super(scope, id, props);
+
+    new NodejsFunction(this, "hello-lambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "handler",
+      entry: join(__dirname, "..", "services", "hello.ts"),
+      environment: {
+        STAGE: props.stageName!,
+      },
+    });
   }
 }
